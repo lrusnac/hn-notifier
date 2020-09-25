5657bbf5f226832075be19de242f196506af73ee
leo <leonidrusnac4@gmail.com>
2020-09-25 19:43:05 +0200

    removed unused constant

--- mail_stories.py
+++ mail_stories.py
@@ -5,7 +5,6 @@ from fs.googledrivefs import GoogleDriveFS
 import os
 
 GOOGLE_DRIVE_SERVICE_ACCOUNT = json.loads(os.environ.get('GOOGLE_DRIVE_SERVICE_ACCOUNT', ''))
-NUM_STORIES = 10
 BASE_URL_HN_API = 'https://hacker-news.firebaseio.com'
 
 NEW_STORIES = 'new_stories'
